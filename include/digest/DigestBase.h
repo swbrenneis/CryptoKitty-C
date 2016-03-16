d16e303daf17f95269e479b063036d6262281b1a
Steve Brenneis <steve.brenneis@seComm.org>
2016-03-15 22:26:05 -0400

    Initial add.

---
+++ include/digest/DigestBase.h
@@ -0,0 +1,41 @@
+#ifndef DIGESTBASE_H_INCLUDED
+#define DIGESTBASE_H_INCLUDED
+
+#include "Digest.h"
+#include "../data/ByteArray.h"
+#include <string>
+
+/*
+ * Digest base implementation class.
+ * The class is abstract. Also includes a convenience method
+ * for creating instances by name.
+ */
+class DigestBase : public Digest {
+
+    protected:
+        DigestBase();
+
+    private:
+        DigestBase(const DigestBase& other);
+        DigestBase& operator= (const DigestBase& other);
+
+    public:
+        virtual ~DigestBase();
+
+    public:
+        ByteArray digest();
+        ByteArray digest(const ByteArray& bytes);
+        void reset();
+        void update(unsigned char byte);
+        void update(const ByteArray& bytes);
+        void update(const ByteArray& bytes, unsigned offset, unsigned length);
+
+    public:
+        static Digest* getInstance(const std::string& algorithm);
+
+    private:
+        ByteArray accumulator;
+
+};
+
+#endif  // DIGESTBASE_H_INCLUDED
