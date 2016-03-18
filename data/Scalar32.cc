9f9aad31f64cfe88b5174a5104fd23db28b70447
Steve Brenneis <steve.brenneis@seComm.org>
2016-03-18 00:14:35 -0400

    Nightly commit.

--- data/Scalar32.cc
+++ data/Scalar32.cc
@@ -1,10 +1,13 @@
 #include "../include/data/Scalar32.h"
+#include "../include/data/OutOfRangeException.h"
 #include <string.h>
 #include <cmath>
 
 // Static initialization
 // Untested = 0, Little endian = 1, big endian = 2
 unsigned char Scalar32::endian = 0;
+const unsigned char Scalar32::LITTLEENDIAN = 1;
+const unsigned char Scalar32::BIGENDIAN = 2;
 
 Scalar32::Scalar32() {
     bytesValid = sValid = uValid = false;
@@ -32,36 +35,74 @@ unsigned Scalar32::asUnsigned() {
     }
     else if (bytesValid) {
         endianTest();
-        if (endian == 1) {
-            u32 = bytes[0];
-            unsigned tmp = bytes[1];
-            tmp = tmp >> 8;
-            u32 |= tmp;
-            tmp = bytes[2];
-            tmp = tmp >> 16;
-            u32 |= tmp;
-            tmp = bytes[3];
-            tmp = tmp >> 24;
-            u32 |= tmp;
-        }
-        else {
-            u32 = bytes[3];
-            unsigned tmp = bytes[2];
-            tmp = tmp >> 8;
-            u32 |= tmp;
-            tmp = bytes[1];
-            tmp = tmp >> 16;
-            u32 |= tmp;
-            tmp = bytes[0];
-            tmp = tmp >> 24;
-            u32 |= tmp;
+        unsigned tmp;
+        switch (endian) {
+            case LITTLEENDIAN:
+                u32 = bytes[0];
+                tmp = bytes[1];
+                tmp = tmp >> 8;
+                u32 |= tmp;
+                tmp = bytes[2];
+                tmp = tmp >> 16;
+                u32 |= tmp;
+                tmp = bytes[3];
+                tmp = tmp >> 24;
+                u32 |= tmp;
+                break;
+            case BIGENDIAN:
+                u32 = bytes[3];
+                tmp = bytes[2];
+                tmp = tmp >> 8;
+                u32 |= tmp;
+                tmp = bytes[1];
+                tmp = tmp >> 16;
+                u32 |= tmp;
+                tmp = bytes[0];
+                tmp = tmp >> 24;
+                u32 |= tmp;
+                break;
+            default:
+                throw OutOfRangeException("Illegal endian value");
         }
     }
     uValid = true;
     return u32;
 
 }
-                    
+                   
+/*
+ * Encode an unsigned integer according to
+ * the endian-ness indicator.
+ */
+unsigned char *Scalar32::encode(unsigned u32, int endian) {
+
+    unsigned char *result = new unsigned char[4];
+    unsigned tmp = u32;
+    switch(endian) {
+        case LITTLEENDIAN:
+            result[0] = tmp & 0xff;
+            tmp = tmp >> 8;
+            result[1] = tmp & 0xff;
+            tmp = tmp >> 8;
+            result[2] = tmp & 0xff;
+            tmp = tmp >> 8;
+            result[3] = tmp & 0xff;
+            break;
+        case BIGENDIAN:
+            result[3] = tmp & 0xff;
+            tmp = tmp >> 8;
+            result[2] = tmp & 0xff;
+            tmp = tmp >> 8;
+            result[1] = tmp & 0xff;
+            tmp = tmp >> 8;
+            result[0] = tmp & 0xff;
+            break;
+        default:
+            throw OutOfRangeException("Illegal endian value");
+    }
+    return result;
+
+}
 
 /*
  * Endian test.
