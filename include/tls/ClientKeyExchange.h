b4c8031596486d93104e80bbc124e9c3191b1652
Steve Brenneis <steve.brenneis@secomm.org>
2016-05-06 00:53:00 +0000

    Initial add.

---
+++ include/tls/ClientKeyExchange.h
@@ -0,0 +1,80 @@
+#ifndef CLIENTKEYEXCHANGE_H_INCLUDED
+#define CLIENTKEYEXCHANGE_H_INCLUDED
+
+#include "tls/HandshakeBody.h"
+#include "tls/Constants.h"
+#include "keys/ECDHKeyExchange.h"
+#include "data/BigInteger.h"
+
+namespace CK {
+    class RSAPrivateKey;
+}
+
+namespace CKTLS {
+
+class ClientKeyExchange : public HandshakeBody {
+
+    public:
+        ClientKeyExchange();
+        ~ClientKeyExchange();
+
+    private:
+        ClientKeyExchange(const ClientKeyExchange& other);
+        ClientKeyExchange& operator= (const ClientKeyExchange& other);
+
+    public:
+        void decode(const CK::ByteArray& encoded);
+        CK::ByteArray encode() const;
+        const CK::BigInteger& getDHPublicKey() const;
+        const CK::ByteArray& getECPublicKey() const;
+        void initState() {}
+        void initState(NamedCurve curve, const CK::ByteArray& pk);
+        void initState(const CK::ECDHKeyExchange::CurveParams& p,
+                                                const CK::ByteArray& pk);
+        void initState(const CK::BigInteger& pk);
+        static void setAlgorithm(KeyExchangeAlgorithm alg);
+
+    private:
+        void decodeDH(const CK::ByteArray& encoded);
+        void decodeECDH(const CK::ByteArray& encoded);
+        CK::ByteArray encodeDH() const;
+        CK::ByteArray encodeECDH() const;
+
+    private:
+        static KeyExchangeAlgorithm algorithm;
+        // ClientDHParams
+        CK::BigInteger dYc;     // D-H public value.
+        // EC parameters
+        ECCurveType curveType;
+        struct ECCurve {
+            CK::BigInteger a;
+            CK::BigInteger b;
+        };
+        // Explicit prime curve type
+        CK::BigInteger primeP;
+        ECCurve curve;
+        CK::BigInteger baseX;
+        CK::BigInteger baseY;
+        CK::BigInteger order;
+        uint32_t cofactor;
+        // Explicit characteristic 2
+        uint16_t m;
+        ECBasisType ebType;
+        // EC trinomial
+        CK::ByteArray k;
+        // EC Pentanomial
+        CK::ByteArray k1;
+        CK::ByteArray k2;
+        CK::ByteArray k3;
+
+        // Named curves
+        NamedCurve named;
+        
+        // Key exchange
+        CK::ByteArray ecPublicKey;
+
+};
+
+}
+
+#endif  // CLIENTKEYEXCHANGE_H_INCLUDED
---
+++ tls/ClientKeyExchange.cc
@@ -0,0 +1,133 @@
+#include "tls/ClientKeyExchange.h"
+#include "data/Unsigned16.h"
+#include "exceptions/tls/RecordException.h"
+#include "exceptions/tls/EncodingException.h"
+
+namespace CKTLS {
+
+// Static initialization.
+KeyExchangeAlgorithm ClientKeyExchange::algorithm;
+
+ClientKeyExchange::ClientKeyExchange() {
+}
+
+ClientKeyExchange::~ClientKeyExchange() {
+}
+
+void ClientKeyExchange::decode(const CK::ByteArray& encoded) {
+
+    switch (algorithm) {
+        case dhe_rsa:
+            decodeDH(encoded);
+            break;
+        case ec_diffie_hellman:
+            decodeECDH(encoded);
+            break;
+        default:
+            throw RecordException("Invalid key exchange algorithm");
+    }
+
+}
+
+void ClientKeyExchange::decodeDH(const CK::ByteArray& encoded) {
+
+        CK::Unsigned16 len(encoded.range(0, 2), CK::Unsigned16::BIGENDIAN);
+        dYc.decode(encoded.range(2, len.getUnsignedValue()), CK::BigInteger::BIGENDIAN);
+
+}
+
+void ClientKeyExchange::decodeECDH(const CK::ByteArray& encoded) {
+}
+
+CK::ByteArray ClientKeyExchange::encode() const {
+
+    CK::ByteArray encoded;
+
+    switch (algorithm) {
+        case dhe_rsa:
+            encoded.append(encodeDH());
+            break;
+        case ec_diffie_hellman:
+            encoded.append(encodeECDH());
+            break;
+        default:
+            throw RecordException("Invalid key exchange algorithm");
+    }
+
+    return encoded;
+
+}
+
+CK::ByteArray ClientKeyExchange::encodeDH() const {
+
+    CK::ByteArray encoded;
+
+    CK::ByteArray pk(dYc.getEncoded(CK::BigInteger::BIGENDIAN));
+    CK::Unsigned16 len(pk.getLength());
+    encoded.append(len.getEncoded(CK::Unsigned16::BIGENDIAN));
+    encoded.append(pk);
+
+    return encoded;
+
+}
+
+CK::ByteArray ClientKeyExchange::encodeECDH() const {
+
+    CK::ByteArray encoded;
+
+    return encoded;
+
+}
+
+const CK::BigInteger& ClientKeyExchange::getDHPublicKey() const {
+
+    return dYc;
+
+}
+
+const CK::ByteArray& ClientKeyExchange::getECPublicKey() const {
+
+    return ecPublicKey;
+
+}
+
+void ClientKeyExchange::initState(const CK::BigInteger& pk) {
+
+    dYc = pk;
+
+}
+
+void ClientKeyExchange::initState(NamedCurve curve, const CK::ByteArray& pk) {
+
+    algorithm = ec_diffie_hellman;
+    curveType = named_curve;
+    named = curve;
+    ecPublicKey = pk;
+
+}
+
+void ClientKeyExchange::initState(const CK::ECDHKeyExchange::CurveParams& params,
+                                                    const CK::ByteArray& pk) {
+
+    algorithm = ec_diffie_hellman;
+    curveType = explicit_prime;
+
+    primeP = params.p;
+    curve.a = params.a;
+    curve.b = params.b;
+    order = params.n;
+    cofactor = params.h;
+    baseX = params.xG;
+    baseY = params.yG;
+
+    ecPublicKey = pk;
+
+}
+
+void ClientKeyExchange::setAlgorithm(KeyExchangeAlgorithm alg) {
+
+    algorithm = alg;
+
+}
+
+}
