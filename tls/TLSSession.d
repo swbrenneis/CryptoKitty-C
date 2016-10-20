07c4ba8371b41d6ec0ff6ac7039deabd7043e48e
Steve Brenneis <steve.brenneis@secomm.org>
2016-10-20 16:16:39 -0400

    Oops.

--- tls/TLSSession.d
+++
@@ -1,3 +0,0 @@
-TLSSession.o: TLSSession.cc ../include/tls/TLSSession.h \
- ../include/tls/../jni/JNIReference.h ../include/tls/TLSCredentials.h \
- ../include/exceptions/TLSException.h ../include/exceptions/Exception.h
