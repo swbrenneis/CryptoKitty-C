83a2240e4d3614030d1597aa56253d0fa648f953
Steve Brenneis <steve.brenneis@secomm.org>
2017-03-14 18:30:37 -0400

    Initial commit.

---
+++ encoding/RSACodec.cc
@@ -0,0 +1,53 @@
+#include "encoding/RSACodec.h"
+#include "keys/RSAPrivateKey.h"
+#include "keys/RSAPublicKey.h"
+#include "cipher/OAEPrsaes.h"
+#include "exceptions/DecryptionException.h"
+#include "exceptions/BadParameterException.h"
+#include "exceptions/EncodingException.h"
+#include "random/FortunaSecureRandom.h"
+
+namespace CK {
+
+RSACodec::RSACodec() {
+
+}
+
+RSACodec::RSACodec(const coder::ByteArray& txt)
+: text(txt) {
+
+}
+
+RSACodec::~RSACodec() {
+
+}
+
+void RSACodec::decrypt(const RSAPrivateKey& privateKey) {
+
+    try {
+        OAEPrsaes cipher(OAEPrsaes::sha256);
+        stream = cipher.decrypt(privateKey, text);
+    }
+    catch (DecryptionException& e) {
+        throw EncodingException(e);
+    }
+
+}
+
+void RSACodec::encrypt(const RSAPublicKey& publicKey) {
+
+    try {
+        OAEPrsaes cipher(OAEPrsaes::sha256);
+        coder::ByteArray seed(32, 0);
+        FortunaSecureRandom rnd;
+        rnd.nextBytes(seed);
+        cipher.setSeed(seed);
+        text = cipher.encrypt(publicKey, stream);
+    }
+    catch (BadParameterException& e) {
+        throw EncodingException(e);
+    }
+
+}
+
+}
